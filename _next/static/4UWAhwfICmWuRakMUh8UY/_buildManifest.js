1489cb92df7a4d4b8cb88832dc5999ca7945d542
ekkans <github@ekkans.com>
2021-08-31 09:26:19 +0000

    Deploying to gh-pages from @ ekkans/next-demo@9b1d0529cd3301f136787ff98f5daf75c9f85380 🚀

---
+++ _next/static/4UWAhwfICmWuRakMUh8UY/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-cecb7aa5d72fad9c3402.js"],"/_error":["static/chunks/pages/_error-737a04e9a0da63c9d162.js"],"/posts/[id]":["static/chunks/pages/posts/[id]-432cdc6ea0ab29c9460b.js"],sortedPages:["/","/_app","/_error","/posts/[id]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
